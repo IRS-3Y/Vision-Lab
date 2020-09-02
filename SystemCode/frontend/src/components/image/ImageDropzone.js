de0e4aa1d9272ab9cd10b3af728bb93e1e3a48af
TS, Yin <tianshi.yin@gmail.com>
2020-09-02 18:25:17 +0800

    Add ImageDropzone component

---
+++ SystemCode/frontend/src/components/image/ImageDropzone.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import {DropzoneArea} from 'material-ui-dropzone';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: '160px'
+  }
+}))
+
+export default function ImageDropzone(props){
+  const classes = useStyles();
+  return (
+    <DropzoneArea
+      dropzoneClass={classes.root}
+      acceptedFiles={['image/*']}
+      filesLimit={1}
+      maxFileSize={10000000}
+      dropzoneText="Drop image here"
+      showPreviews={false}
+      showPreviewsInDropzone={false}
+      showAlerts={false}
+      {...props}/>
+  )
+}
