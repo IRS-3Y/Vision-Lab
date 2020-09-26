f045651b92a8b9e98ad530b9b5676eb3b8b757db
TS, Yin <tianshi.yin@gmail.com>
2020-09-26 15:31:26 +0800

    Use ResNet50 v2

--- SystemCode/backend/app/models/resnet50.py
+++ SystemCode/backend/app/models/resnet50.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications import ResNet50V2
+from tensorflow.keras.applications.resnet_v2 import preprocess_input
 
 from . import _utils
 
@@ -17,7 +17,7 @@ def load_model(model_version = 'default'):
 def build_model():
   inputs = Input(shape = (IMAGE_SIZE, IMAGE_SIZE, 3))
   x = preprocess_input(inputs)
-  x = ResNet50(weights=None, classes=2)(x)
+  x = ResNet50V2(weights=None, classes=2)(x)
   model = Model(inputs=inputs, outputs=x)
   model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
   return model
