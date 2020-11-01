64adbffe00b07ac10e3c2e04892b11bf581d36c9
TS, Yin <tianshi.yin@gmail.com>
2020-11-01 18:29:55 +0800

    Rebuild Docker images

--- SystemCode/backend/docker/core/setup.py
+++ SystemCode/backend/docker/core/setup.py
@@ -5,9 +5,11 @@ from .models import create_model
 
 if count(Model) == 0:
   # add preloaded detector models
-  create_model(uuid=uuid4(), type='detector', name='ensemble_stacking224', version='preload_1', label='Preloaded (Stacking Ensemble)', status=1, ensemble=1)
-  create_model(uuid=uuid4(), type='detector', name='experiment', version='preload_1', label='Preloaded (Experiment)', status=1, ensemble=0)
-  create_model(uuid=uuid4(), type='detector', name='resnet50v2', version='preload_1', label='Preloaded (ResNet50 v2)', status=1, ensemble=0)
+  create_model(uuid=uuid4(), type='detector', name='ensemble_stacking224', version='preload_9915', label='Preloaded (Stacking Ensemble)', status=1, ensemble=1)
+  create_model(uuid=uuid4(), type='detector', name='xception', version='preload_9806', label='Preloaded (Xception)', status=1, ensemble=0)
+  create_model(uuid=uuid4(), type='detector', name='densenet121', version='preload_9477', label='Preloaded (DenseNet121)', status=1, ensemble=0)
+  create_model(uuid=uuid4(), type='detector', name='mobilenet_v2', version='preload_9576', label='Preloaded (MobileNet v2)', status=1, ensemble=0)
+  create_model(uuid=uuid4(), type='detector', name='nasnet_mobile', version='preload_9467', label='Preloaded (NASNet Mobile)', status=1, ensemble=0)
 
   # add preloaded generator models
   create_model(uuid=uuid4(), type='generator', name='stylegan2', version='preload_asian_face', label='Asian Face (StyleGAN)', status=1, ensemble=0)
